f60d00d5ca82ed44bab63cef059a160d712bdec4
RayRay <104270870+4Cloudee@users.noreply.github.com>
2025-08-17 21:14:40 +0200

    Delete kubejs directory
    - stuff

--- kubejs/server_scripts/tempad/tags.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.tags('fluid', event => {
-    event.add('tempad:tempad_liquid_fuel', 'mekanismgenerators:fusion_fuel')
-    event.remove('tempad:tempad_liquid_fuel',"minecraft:lava")
-})
--- kubejs/startup_scripts/mekanism/custom_items.js
+++
@@ -1,3 +0,0 @@
-StartupEvents.registry('item', event => {
-	event.create('mekanism:incomplete_basic_control_circuit', 'create:sequenced_assembly')
-})
